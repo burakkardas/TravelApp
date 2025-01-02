6b48765264b055095b211d804e7f383e51521880
Burak Kardaş <krdsbrk@gmail.com>
2025-01-03 00:31:52 +0300

    Initial Commit

---
+++ Travel App/Travel_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_AppApp.swift
+//  Travel App
+//
+//  Created by Burak on 3.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct Travel_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
